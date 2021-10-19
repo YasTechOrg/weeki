6e72f74e12c6ba43dd1934eecf7da105f255f8f2
Hossein Araghi <hoseinaraghi84@gmail.com>
2021-10-19 14:29:58 +0330

    Add Auth Actions In Vuex

--- src/client/src/store/index.ts
+++ src/client/src/store/index.ts
@@ -1,12 +1,41 @@
 import { createStore } from 'vuex'
+import createPersistedState from "vuex-persistedstate"
 
 export default createStore({
   state: {
+    userData: {},
+    at: "",
+    at_time: ""
   },
+
+  plugins: [createPersistedState()],
+
   mutations: {
+
+    setAuth(state, token)
+    {
+      state.at = token
+      state.at_time = String(new Date())
+    },
+
+    removeAuth(state)
+    {
+      state.at = ""
+      state.at_time = ""
+      state.userData = {}
+    },
+
+    setUserData(state, data)
+    {
+      state.userData = data
+    },
   },
-  actions: {
+
+  getters: {
+
+    getAuth: state =>
+    {
+      return `Bearer ${state.at}`
+    },
   },
-  modules: {
-  }
 })
